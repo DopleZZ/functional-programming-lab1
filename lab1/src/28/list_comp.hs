025cf8931875f034e29d0bb4021dec1c0352edb7
DopleZZ <tema.bond@outlook.com>
2025-11-20 10:24:31 +0300

    changes

--- lab1/src/28/list_comp.hs
+++ lab1/src/28/list_comp.hs
@@ -5,11 +5,7 @@ target = 1001
 
 main :: IO ()
 main =
-  print $
-    1
-      + sum
-        [ s * s - m * (s - 1)
-        | k <- [1 .. (target - 1) `div` 2],
-          let s = 2 * k + 1,
-          m <- [0 .. 3]
-        ]
+  print $ summi 1
+
+summi :: Integer -> Integer
+summi n = n + sum [s * s - m * (s - 1) | k <- [1 .. (target - 1) `div` 2], let s = 2 * k + 1, m <- [0 .. 3]]
